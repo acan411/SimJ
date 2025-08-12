5b69868803b1169cf2dff908719eff5d0e517889
acan <opsitesky@163.com>
2025-08-12 16:40:35 +0800

    desmoj

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SimJ
 离散事件仿真学习
+
+## 执行步骤
+1. 执行[installDesmoj.bat](installDesmoj.bat)
+2. 同步maven
---
+++ installDesmoj.bat
@@ -0,0 +1,7 @@
+mvn install:install-file ^
+	-Dfile=lib/desmoj-2.5.1e-bin.jar ^
+	-DgroupId=desmoj ^
+	-DartifactId=desmoj ^
+	-Dversion="2.5.1e" ^
+	-Dpackaging=jar ^
+	-DgeneratePom=true
